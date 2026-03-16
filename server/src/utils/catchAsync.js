158a1fdd32ec33025b336d4d3e5d7bf0d49c86f4
Prisans <124236508+Prisans@users.noreply.github.com>
2026-03-17 02:40:40 +0530

    Fix: diagnostic logs in catchAsync

--- server/src/utils/catchAsync.js
+++ server/src/utils/catchAsync.js
@@ -1,6 +1,13 @@
 const catchAsync = (fn) => (req, res, next) => {
+  if (typeof next !== "function") {
+    console.error("CatchAsync: next is not a function!");
+  }
   Promise.resolve(fn(req, res, next)).catch((err) => {
-    next(err);
+    if (typeof next === "function") {
+      next(err);
+    } else {
+      console.error("CatchAsync: Cannot call next(err) because next is not a function", err);
+    }
   });
 };
 
